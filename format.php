afa8d446625f2271d30a55dab65c0411e5f848a4
Daniel Thies <dthies@ccal.edu>
2018-03-01 13:00:58 -0600

    Initial commit

---
+++ format.php
@@ -0,0 +1,114 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Code for switching question types when importing Moodle XML.
+ *
+ * @package    qformat_crossxml
+ * @copyright  Daniel Thies <dethies@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/question/format/xml/format.php');
+
+/**
+ * Importer for Cross XML question format.
+ *
+ * See http://docs.moodle.org/en/Moodle_XML_format for a description of the format.
+ *
+ * @copyright  1999 onwards Martin Dougiamas {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qformat_crossxml extends qformat_xml {
+
+    public function provide_import() {
+        return true;
+    }
+
+    public function provide_export() {
+        return false;
+    }
+
+    // Override to change question object to short answer.
+    public function import_multichoice($question) {
+        $qo = parent::import_multichoice($question);
+        $qo->qtype = 'shortanswer';
+        for ($k = 0; $k < count($qo->answer); $k++) {
+            if ($qo->answer[$k]['format'] == FORMAT_HTML) {
+                $qo->answer[$k] = html_to_text($qo->answer[$k]['text']);
+            } else {
+                $qo->answer[$k] = $qo->answer[$k]['text'];
+            }
+        }
+        return $qo;
+    }
+
+    // Override to change question object to multichoice.
+    public function import_shortanswer($question) {
+        $qo = parent::import_shortanswer($question);
+        $qo->qtype = 'multichoice';
+        $qo->single = 1;
+        for ($k = 0; $k < count($qo->answer); $k++) {
+            $qo->answer[$k] = array('text' => $qo->answer[$k], 'format' => FORMAT_PLAIN);
+        }
+        return $qo;
+    }
+
+    public function import_match($question) {
+        $qo = parent::import_match($question);
+        if (in_array('ddmatch', core_component::get_plugin_list('qtype'))) {
+            $qo->qtype = 'ddmatch';
+            for ($k = 0; $k < count($qo->subanswers); $k++) {
+                $qo->subanswers[$k] = array(
+                    'text' => text_to_html($qo->subanswers[$k]),
+                    'format' => FORMAT_HTML
+                    //'text' => $qo->subanswers[$k],
+                    //'format' => FORMAT_PLAIN
+                );
+            }
+        }
+        return $qo;
+    }
+
+    protected function import_question($questionxml) {
+        $questiontype = $questionxml['@']['type'];
+
+        if ($questiontype != 'ddmatch' ) {
+            return parent::import_question($questionxml);
+        }
+
+        if (!in_array('ddmatch', core_component::get_plugin_list('qtype'))) {
+            return null;
+        }
+        $qtype = question_bank::get_qtype('ddmatch', false);
+
+        $qo = $qtype->import_from_xml($questionxml, null, $this);
+        $qo->qtype = 'match';
+        for ($k = 0; $k < count($qo->subanswers); $k++) {
+            if ($qo->subanswers[$k]['format'] == FORMAT_HTML) {
+                $qo->subanswers[$k] = html_to_text($qo->subanswers[$k]['text']);
+            } else {
+                $qo->subanswers[$k] = $qo->subanswers[$k]['text'];
+            }
+        }
+        return $qo;
+
+    }
+
+}
---
+++ tests/behat/import.feature
@@ -0,0 +1,29 @@
+@qformat @qformat_crossxml
+Feature: Test importing questions from Moodle glossary export.
+  In order to reuse some questions
+  As an teacher
+  I need to be able to import them in an alternate form.
+
+  Background:
+    Given the following "courses" exist:
+      | fullname | shortname | format |
+      | Course 1 | C1        | topics |
+    And the following "users" exist:
+      | username | firstname |
+      | teacher  | Teacher   |
+    And the following "course enrolments" exist:
+      | user    | course | role           |
+      | teacher | C1     | editingteacher |
+    And I log in as "teacher"
+    And I follow "C1"
+
+  @javascript @_file_upload
+  Scenario: import XML file of an export glossary
+    When I navigate to "Import" node in "Course administration > Question bank"
+    And I set the field "id_format_crossxml" to "1"
+    And I upload "question/format/crossxml/tests/fixtures/glossary.xml" file to "Import" filemanager
+    And I press "id_submitbutton"
+    Then I should see "Parsing questions from import file."
+    And I should see "Importing 2 questions from file"
+    When I press "Continue"
+    Then I should see "Which Roman general"
